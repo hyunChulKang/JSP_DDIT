ca3f87b1e7c34f6f505cda004fdbb27fd8508f52
HyunChulKang <godapple89@nate.com>
2020-04-07 18:38:56 +0900

    JSP 수업내용

---
+++ src/com/jsp/dao/MemberDAOImpl.java
@@ -0,0 +1,77 @@
+package com.jsp.dao;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import com.jsp.dto.MemberVO;
+import com.jsp.mybatis.OracleMyBatisSqlSessionFactoryBuilder;
+
+public class MemberDAOImpl implements MemberDAO{
+	SqlSessionFactory sqlSessionFactory;
+	private static MemberDAOImpl memberDao;
+	private MemberDAOImpl() {
+		sqlSessionFactory= OracleMyBatisSqlSessionFactoryBuilder.getSqlSessionFactory();
+	}
+	public static MemberDAOImpl getMemberDaoImpl() {
+		if(memberDao ==null) {
+			memberDao= new MemberDAOImpl();
+			return memberDao;
+		}
+		return memberDao;
+	}
+	
+	
+	private SqlSessionFactory sessionFactory =OracleMyBatisSqlSessionFactoryBuilder.getSqlSessionFactory(); 
+
+	@Override
+	public List<MemberVO> selectMemberList() throws SQLException {
+		SqlSession session = sessionFactory.openSession();
+		List<MemberVO> memberList = session.selectList("Member-Mapper.selectMemberList",null);
+		session.close();
+		return memberList;
+	}
+
+	@Override
+	public int selectMemberListCount() throws SQLException {
+		int count=0;
+		SqlSession session=sessionFactory.openSession();
+		count=session.selectOne("Member-Mapper.selectMemberListCont",null);
+		session.close();
+		return count;
+	}
+
+	@Override
+	public MemberVO selectMemberById(String id) throws SQLException {
+		SqlSession session=sessionFactory.openSession();
+		MemberVO member=session.selectOne("Member-Mapper.selectMemberById",id);
+		session.close();
+		return member;
+	}
+	/* DML이기때문에 commit이 필요!
+	 * session안에 있는 오토 컴잇이 false로 고정되어있어서 true로 변경하여 commit한다   .*/
+	@Override
+	public void insertMember(MemberVO member) throws SQLException {
+		SqlSession session =sessionFactory.openSession(true);
+		session.update("Member-Mapper.insertMember",member);
+		session.close();
+	}
+
+	@Override
+	public void updateMember(MemberVO member) throws SQLException {
+		SqlSession session= sessionFactory.openSession(true);
+		session.update("Member-Mapper.updateMember",member);
+		session.close();
+	}
+
+	@Override
+	public void deleteMember(String id) throws SQLException {
+		SqlSession session= sessionFactory.openSession(true);
+		session.update("Member-Mapper.updateMember",id);
+		session.close();
+	}
+
+}
---
+++ src/com/jsp/service/MemberServiceImpl.java
@@ -0,0 +1,53 @@
+package com.jsp.service;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import com.jsp.dao.MemberDAO;
+import com.jsp.dao.MemberDAOImpl;
+import com.jsp.dto.MemberVO;
+import com.jsp.exception.InvalidPasswordException;
+import com.jsp.exception.NotFoundIDExcepiton;
+
+public class MemberServiceImpl implements MemberService{
+
+	private MemberDAO memberDAO=MemberDAOImpl.getMemberDaoImpl();
+	private static MemberServiceImpl instance = new MemberServiceImpl();
+	private MemberServiceImpl() {}
+	public static MemberServiceImpl getInstance() {
+		return instance;
+	}
+	
+	@Override
+	public void login(String id, String pwd) throws SQLException, NotFoundIDExcepiton, InvalidPasswordException {
+	MemberVO member = memberDAO.selectMemberById(id);
+	if(member ==null) throw new NotFoundIDExcepiton();
+	if(!pwd.equals(member.getPwd())) throw new InvalidPasswordException();
+	}
+
+	@Override
+	public List<MemberVO> getMemberList() throws SQLException {
+		return memberDAO.selectMemberList();
+	}
+
+	@Override
+	public MemberVO getMember(String id) throws SQLException {
+		return memberDAO.selectMemberById(id);
+	}
+
+	@Override
+	public void regist(MemberVO member) throws SQLException {
+		memberDAO.insertMember(member);
+	}
+
+	@Override
+	public void modify(MemberVO member) throws SQLException {
+		memberDAO.updateMember(member);
+	}
+
+	@Override
+	public void remove(String id) throws SQLException {
+		memberDAO.deleteMember(id);
+	}
+
+}
